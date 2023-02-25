1c48184268eb9303e27cae88e7051628d1671909
Erasmo Junior <erasmo.cjunior@gmail.com>
2023-02-25 20:30:57 -0300

    :wrench: CHORE: Query sql e finalizando modulo

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
 
-app.get('/', (req, res) => {
-	res.send('<h1>Full Cycle</h1>')
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Limpa Telas')`
+connection.query(sql)
+connection.end()
+
+
+app.get('/', (req,res) => {
+    res.send('<h1>Full Cycle</h1>')
 })
 
 app.listen(port, ()=> {
-	console.log('Rodando na porta: '+ port)
+    console.log('Rodando na porta ' + port)
 })
