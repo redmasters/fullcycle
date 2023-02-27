6243b719789918b44167b74c2ad4759fa468f907
Erasmo Jr <erasmo.cjunior@gmail.com>
2023-02-27 13:52:23 -0300

    :construction: WIP: Adicionando configuracoes de banco de dados

--- docker/desafio/nginxNode/node/index.js
+++ docker/desafio/nginxNode/node/index.js
@@ -2,31 +2,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const config = {
-	host: 'db',
-	user: 'root',
-	password: 'root',
-	database: 'nodedb'
+
+async function createTable () {
+  const db = require('./db')
+  const connection = await db.connect();
+  const createIfNotExists =
+    'CREATE TABLE IF NOT EXISTS people (id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))'
+  return await connection.query(createIfNotExists);
 }
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+//
+// async function insertPerson (name) {
+//   const connection = await connect();
+//   const sql = 'INSERT INTO people(name) VALUES(?)'
+//   const values = [name]
+//   return await connection.query(sql, values);
+// }
+//
+// async function selectPeople () {
+//   const connection = await connect();
+//
+//   const [rows] = await connection.query('SELECT * FROM people')
+//   return rows;
+// }
 
-const query = `
-INSERT INTO people(nome) value('Carlos');
-`
-connection.query(query)
-connection.end()
+createTable().then(r => console.log(r));
+
 
 const html = '<h1> Full Cycle Rocks!! Faz o L!</h1> <br>' +
-	' lista de pessoas: <br>' +
+  ' lista de pessoas: <br>' +
 
-
-app.get('/', (req, res) => {
-	res.send(html)
-})
-
+  app.get('/', (req, res) => {
+    res.send(html)
+  })
 
 app.listen(port, () => {
-	console.log('Rodando na porta ' + port )
+  console.log('Rodando na porta ' + port)
 })
