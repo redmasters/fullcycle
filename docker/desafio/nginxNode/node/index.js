8dfdc3bbdcedc3849b6acfe130a5cb50b6938eb4
Erasmo Junior <erasmo.cjunior@gmail.com>
2023-02-28 00:16:39 -0300

    :sparkles: FEAT: Concluido parcial: Node

--- docker/desafio/nginxNode/node/index.js
+++ docker/desafio/nginxNode/node/index.js
@@ -2,38 +2,59 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mysql = require('mysql')
 
-async function createTable () {
-  const db = require('./db')
-  const connection = await db.connect();
-  const createIfNotExists =
-    'CREATE TABLE IF NOT EXISTS people (id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))'
-  return await connection.query(createIfNotExists);
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
 }
+const connection = mysql.createConnection(config)
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+    return
+  }
+  console.log('Conectado usando thread ID: ' + connection.threadId)
+})
 
-//
-// async function insertPerson (name) {
-//   const connection = await connect();
-//   const sql = 'INSERT INTO people(name) VALUES(?)'
-//   const values = [name]
-//   return await connection.query(sql, values);
-// }
-//
-// async function selectPeople () {
-//   const connection = await connect();
-//
-//   const [rows] = await connection.query('SELECT * FROM people')
-//   return rows;
-// }
+connection.query(
+  'CREATE TABLE IF NOT EXISTS people (id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))',
+  (err) => {
+    if (err) {
+      console.error(err)
+    }
+    console.log('Tabela criada com sucesso')
+  }
+)
 
-createTable().then(r => console.log(r));
+const name = 'Erasmo Full Cycle'
+const sql = 'INSERT INTO people(name) VALUES(?)'
+const values = [name]
+connection.query(sql, values)
 
+const query = 'SELECT * FROM people'
 
-const html = '<h1> Full Cycle Rocks!! Faz o L!</h1> <br>' +
-  ' lista de pessoas: <br>' +
+const html = '<header>' +
+  '<title> Full Cycle - Desafio Node + Nginx </title>' +
+  '</header>' +
+  '<h1> Full Cycle Rocks!!' +
+  '<span style="font-size:100%;color:red;">&starf;</span>\n</h1> <br> ' +
+  '<strong> Lista de nomes cadastrados no banco de dados:</strong> <br>'
 
   app.get('/', (req, res) => {
-    res.send(html)
+    try {
+      connection.query(query, (err, results) => {
+        if (err) {
+          console.error(err)
+        }
+        res.send(html + results.map((result) =>
+          result.name).join('<br>') + '<br>')
+      })
+    } catch (error) {
+      console.error(error)
+    }
   })
 
 app.listen(port, () => {
