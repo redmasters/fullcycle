11d5bd602ef4712a9542c7fbe41ab96d24fb09e3
Erasmo Junior <erasmo.cjunior@gmail.com>
2024-06-29 18:51:42 -0300

    :sparkles: FEAT: Encadeamento de categorias

--- graphQl/go/internal/database/category.go
+++ graphQl/go/internal/database/category.go
@@ -43,3 +43,15 @@ func (c *Category) FindAll() ([]Category, error) {
 	}
 	return categories, nil
 }
+
+func (c *Category) FindByCourseID(courseID string) (Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("SELECT c.id, c.name, c.description "+
+		"FROM categories c "+
+		"JOIN courses cr "+
+		"ON c.id = cr.category_id WHERE cr.id = $1", courseID).Scan(&id, &name, &description)
+	if err != nil {
+		return Category{}, err
+	}
+	return Category{ID: id, Name: name, Description: description}, nil
+}
