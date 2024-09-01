9b95b519e1636dfe038dd8a14888f6891c6b216d
Erasmo Junior <erasmo.cjunior@gmail.com>
2024-09-01 00:05:58 -0300

    :construction: WIP: Curso: Kubernetes. Capitulo: Objetos de configuracao
    :white_check_mark: Concluido: Injetando ConfigMap na aplicação

--- kubernetes/server.go
+++ kubernetes/server.go
@@ -1,10 +1,15 @@
 package main
-import "net/http"
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
 
 
 func main() {
+  http.HandleFunc("/configmap", ConfigMap)
   http.HandleFunc("/", Hello)
   http.ListenAndServe(":8000", nil)
 }
@@ -16,5 +21,16 @@ func Hello(w http.ResponseWriter, r *http.Request) {
   name2:= os.Getenv("NAME2")
 
   fmt.Fprintf(w, "%s e %s sao duas angels", name1, name2)
+
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+  data, err := ioutil.ReadFile("/myfamily/myfamily.txt")
+  if err != nil {
+    fmt.Println("File reading error", err)
+  } 
+
+  fmt.Fprintf(w, "My family: %s.", string(data))
 
 }
